69a6e262deb7718216ab910d95ad27457e782bba
unknown <harvy1997@gmail.com>
2024-03-08 06:36:35 -0500

    cambios

--- src/api/recharge-product/controllers/recharge-product.js
+++ src/api/recharge-product/controllers/recharge-product.js
@@ -6,4 +6,25 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::recharge-product.recharge-product');
+module.exports = createCoreController('api::recharge-product.recharge-product',
+// ({strapi})=>({
+//     async sumByGroup(ctx) {
+//         try {
+//           // Perform a custom query to aggregate the sum grouped by the specified field
+//           const result = await strapi.query('recharge-product').model.aggregate([
+//             {
+//               $group: {
+//                 _id: '$groupField', // Field to group by
+//                 total: { $sum: '$sumField' } // Field to sum
+//               }
+//             }
+//           ]);
+    
+//           return result;
+//         } catch (error) {
+//           console.error('Error:', error);
+//           return ctx.response.internalServerError('An error occurred while processing the request.');
+//         }
+//       },
+// })
+);
